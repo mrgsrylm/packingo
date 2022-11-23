068b0a566a6de2c48543469c99a6b39263592863
Agus S. Mubarok <gusrylmubarok@gmail.com>
2022-11-23 09:12:21 +0700

    Integrate User or Customer Template

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,7 +16,13 @@ class HomeController extends Controller
         if ($usertype === '1') {
             return view('admin.home');
         }else{
-            return view('dashboard');
+            return view('home.userpage');
         }
+    }
+
+    // Home Controller
+    public function index()
+    {
+        return view('home.userpage');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+route::get('/', [HomeController::class, 'index']);
 
 Route::middleware([
     'auth:sanctum',
